8772ef10066097423fd1e6f7dea0682d32c6f6ce
Dominik Peters <kuhlesdominik@gmx.de>
2022-07-12 22:47:09 +0200

    update user.js to have update url point to meta file

--- ActionOverview.user.js
+++ ActionOverview.user.js
@@ -3,7 +3,7 @@
 // @match            https://www.milkywayidle.com/game
 // @run-at           document-start
 // @grant            none
-// @updateURL        https://github.com/Bl00D4NGEL/Milky-Way-Idle-Scripts/raw/main/ActionOverview.user.js
+// @updateURL        https://github.com/Bl00D4NGEL/Milky-Way-Idle-Scripts/raw/main/ActionOverview.meta.js
 // @downloadURL      https://github.com/Bl00D4NGEL/Milky-Way-Idle-Scripts/raw/main/ActionOverview.user.js
 // @description      Adds a small overview of the current action/s
 // @version          0.1.3
