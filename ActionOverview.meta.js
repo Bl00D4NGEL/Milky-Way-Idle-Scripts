ab486b33fdb3b929f64df5f724fa0d3b6962350d
Dominik Peters <kuhlesdominik@gmx.de>
2022-07-12 22:41:41 +0200

    Create ActionOverview.meta.js

---
+++ ActionOverview.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name             Milky way action overview
+// @match            https://www.milkywayidle.com/game
+// @run-at           document-start
+// @grant            none
+// @updateURL        https://github.com/Bl00D4NGEL/Milky-Way-Idle-Scripts/raw/main/ActionOverview.meta.js
+// @downloadURL      https://github.com/Bl00D4NGEL/Milky-Way-Idle-Scripts/raw/main/ActionOverview.user.js
+// @description      Adds a small overview of the current action/s
+// @version          0.1.2
+// ==/UserScript==
